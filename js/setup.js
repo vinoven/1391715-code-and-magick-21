6479f47794aab861d3a09e34250caf9bf3f0a0cd
vinoven <mikhailsechin@gmail.com>
2020-09-30 02:24:36 +0300

    Добавляет генерацию вида волшебников

--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,5 @@
 'use strict';
+//  Входные данные
 
 const FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 const SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
@@ -14,3 +15,25 @@ const EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
 const userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
+
+//  Рандомайзер
+
+const getRandom = (items) => {
+  const item = items[Math.floor(Math.random() * items.length)];
+  return item;
+};
+
+//  Генерация вида волшебников
+
+const similarWizard = (firstNames, secondNames, coatColors, eyesColors) => {
+  let wizard = {
+    name: getRandom(firstNames) + ' ' + getRandom(secondNames),
+    coatColor: getRandom(coatColors),
+    eyesColor: getRandom(eyesColors)
+  };
+  return wizard;
+};
+
+for (var i = 0; i < 4; i++) {
+  similarWizard(FIRST_NAMES, SECOND_NAMES, COAT_COLORS, EYES_COLORS);
+}
