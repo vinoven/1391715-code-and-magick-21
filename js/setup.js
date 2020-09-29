f22766d4e806093485959c8d17059a1a85bcdf84
vinoven <mikhailsechin@gmail.com>
2020-09-30 01:59:11 +0300

    Добавляет входные данные для генерации волшебников

--- js/setup.js
+++ js/setup.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+const SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+const COAT_COLORS = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+const EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+const userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
